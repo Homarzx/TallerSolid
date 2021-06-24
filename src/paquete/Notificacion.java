e332c92b6fbaf7fa0cd2c36833cae32b6c1ae7e1
alejandraquimi <alevquim@espol.edu.ec>
2021-06-24 17:41:08 -0500

    OCP en la clase notificacion

--- src/paquete/Notificacion.java
+++ src/paquete/Notificacion.java
@@ -12,3 +12,12 @@ public class Notificacion {
 	}
 
 }
+
+
+/*En este ejemplo se viola el Open Closed Principle porque no estan abiertas a la extension es decir 
+al querer agregar una nueva forma de notificacion(Telegram), tendria que modificar esta clase(Notificacion) para
+que realice la notificacion para Telegram.
+La solucion seria Seria mejor implementar una interface en este caso Tipo que tenga el método abstracto notificar(),
+entonces al agregar una nueva notificacion como Telegram esta implementara esta interfaz junto con el método. 
+ 		
+*/
--- src/paquete/Pago.java
+++ src/paquete/Pago.java
@@ -10,5 +10,29 @@ public class Pago {
 	public void generarFactura() {
 		
 	}
+	
 
 }
+
+/* En este ejemplo se viola el principio de "Liskov Substitution" que dice que cada clase que hereda de otra puede utilizarse como su clase padre 
+	sin conocer necesariamente las diferencias que hay entre ellas. La solución sería la siguiente:
+	public class Pago{
+           private boolean LoggedIn;
+           public void realizarCobro(double monto){   
+           }
+           public void realizarCobroPayPal(double monto){
+              if(!LoggedIn){
+               return;
+              }
+           }
+           public void calcularImpuestosFactura(){
+       
+           }
+           public void generarFacturas(){
+       
+           }
+   
+       }
+	
+	*/
+
