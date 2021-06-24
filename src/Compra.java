aa6e14e487157d88e758cd1504235ed1ddf15d56
DennisseAguirre <85047464+DennisseAguirre@users.noreply.github.com>
2021-06-24 10:34:54 -0500

    solid Single-Responsibility Principle

--- src/Compra.java
+++ src/Compra.java
@@ -16,5 +16,21 @@ public class Compra {
 	public void removerArticulo(Articulo articulo) {
 		
 	}
+	/*
+        En este ejemplo se viola el principio de "Single responsibility principle", que dice que cada clase debe tener sólo una única responsabilidad , por lo tanto si una
+	clase se ocupa de varias tareas es mejor separar ocupaciones mediante otras clases. La solución sería crear una clase de Artículos y poner los dos métodos
+	relacionados.
+	 public class Articulo{
+             private List articulos;
+    
+              public void agregarArticulo(Articulo articulo){
+        
+              }
+              public void removerArticulo(Articulo articulo){
+        
+               }
+}
+        
+        */
 
 }
